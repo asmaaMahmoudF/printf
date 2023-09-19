2a3c45e279046a4f7306d7de9cbbcac6e1274d67
asmaaMahmoudF <your@example.com>
2023-09-18 19:39:55 -0700

    functions of printf

--- main_printf.h
+++ main_printf.h
@@ -1,5 +1,5 @@
-#ifndef main_printf_H
-#define main_printf_H
+#ifndef MAIN_PRINTF_H
+#define MAIN_PRINTF_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,17 +11,19 @@
  * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
  * @f: type pointer to function for the conversion specifier
  *
- */
-
+*/
 typedef struct format
 {
-        char *id;
-        int (*f)();
-} convert_match;
+char *id;
+int (*f)();
 
+}
+convert_match;
 
 int _printf(const char *format, ...);
 int _putchar(char ch);
-
+int printf_string(va_list val);
+int printf_char(va_list val);
+int printf_dec(va_list args);
+int printf_37(void);
 #endif
-~      
---
+++ printf_char.c
@@ -0,0 +1,15 @@
+#include "main_printf.h"
+/**
+* printf_char -  print character
+* @val: input
+* Return: char
+*/
+int printf_char(va_list val)
+{
+char c;
+
+s = va_arg(val, int);
+_putchar(s);
+return (1);
+}
+
