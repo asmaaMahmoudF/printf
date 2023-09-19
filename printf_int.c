924d63fbf63b9898c816003b30a97f989138e047
489Esraa <asraaalaa2050@gmail.com>
2023-09-19 07:07:52 -0700

    esraaalaamohamed

--- printf_int.c
+++ printf_int.c
@@ -1,4 +1,5 @@
-#include "mai.h"
+#include "main.h"
+
 /**
  * printf_int - prints integer
  * @args: argument to print
@@ -6,41 +7,39 @@
  */
 int printf_int(va_list args)
 {
-	int n = va_arg(args, int);
-	int num, last = n % 10, digit, exp = 1;
-	int  i = 1;
+int n = va_arg(args, int);
+int num, last = n % 10, digit, exp = 1;
+int  i = 1;
 
-	n = n / 10;
-	num = n;
-
-	if (last < 0)
-	{
-		_putchar('-');
-		num = -num;
-		n = -n;
-		last = -last;
-		i++;
-	}
-	if (num > 0)
-	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
-		}
-	}
-	_putchar(last + '0');
-
-	return (i);
+n = n / 10;
+num = n;
+if (last < 0)
+{
+_putchar('-');
+num = -num;
+n = -n;
+last = -last;
+i++;
+}
+if (num > 0)
+{
+while (num / 10 != 0)
+{
+exp = exp * 10;
+num = num / 10;
+}
+num = n;
+while (exp > 0)
+{
+digit = num / exp;
+_putchar(digit + '0');
+num = num - (digit *exp);
+exp = exp / 10;
+i++;
+}
+}
+_putchar(last + '0');
+return (i);
 }
 
 /**
@@ -51,40 +50,38 @@ int printf_int(va_list args)
 
 int printf_dec(va_list args)
 {
-	int n = va_arg(args, int);
-	int num, last = n % 10, digit;
-	int  i = 1;
-	int exp = 1;
+int n = va_arg(args, int);
+int num, last = n % 10, digit;
+int  i = 1;
+int exp = 1;
 
-	n = n / 10;
-	num = n;
-
-	if (last < 0)
-	{
-		_putchar('-');
-		num = -num;
-		n = -n;
-		last = -last;
-		i++;
-	}
-	if (num > 0)
-	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			digit = num / exp;
-			_putchar(digit + '0');
-			num = num - (digit * exp);
-			exp = exp / 10;
-			i++;
-		}
-	}
-	_putchar(last + '0');
-
-	return (i);
+n = n / 10;
+num = n;
+if (last < 0)
+{
+_putchar('-');
+num = -num;
+n = -n;
+last = -last;
+i++;
+}
+if (num > 0)
+{
+while (num / 10 != 0)
+{
+exp = exp * 10;
+num = num / 10;
+}
+num = n;
+while (exp > 0)
+{
+digit = num / exp;
+_putchar(digit + '0');
+num = num - (digit *exp);
+exp = exp / 10;
+i++;
+}
+}
+_putchar(last + '0');
+return (i);
 }
--- printf_string.c
+++ printf_string.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
 * printf_string - printf string
 * @val: input
