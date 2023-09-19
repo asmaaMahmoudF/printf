1b7899a70559b0b53044d2905316032f80611314
asmaaMahmoudF <your@example.com>
2023-09-19 08:00:44 -0700

    Asmaa

---
+++ _strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
--- main.h
+++ main.h
@@ -22,7 +22,7 @@ int (*f)();
 convert_match;
 
 int _printf(const char *format, ...);
-int _putchar(char chnclude <unistd.h>);
+int _putchar(char ch);
 int _strlen(char *s);
 int printf_string(va_list val);
 int printf_char(va_list val);
