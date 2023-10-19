bbc59108e2008f755026fd539e1607e9c6d3e6a5
asmaaMahmoudF <asmaafahmy932@gmail.com>
2023-10-19 20:08:44 +0300

    “asss”

--- printf_unsigned.c
+++ printf_unsigned.c
@@ -1,44 +1,68 @@
 #include "main.h"
 /**
- * printf_unsigned - prints integer
- * @args: argument to print
- * Return: number of characters printed
+ * print_unsigned_help - print intgers
+ * @n: number to be printed
+ * Return: length of int
  */
-int printf_unsigned(va_list args)
+int print_unsigned_help(unsigned int n)
 {
-	unsigned int n = va_arg(args, unsigned int);
-	int nums, last = n % 10, digit, exp = 1;
-	int  i = 1;
+	unsigned int digit, ncpy, remainder, reverse = 0;
+	int len = 0, i = 0;
 
-	n = n / 10;
-	nums = n;
-
-	if (last < 0)
+	if (n == 0)
 	{
-		_putchar('-');
-		nums = -nums;
-		n = -n;
-		last = -last;
+		_putchar('0');
+		return (1);
+	}
+	ncpy = n;
+
+	while (ncpy > 0)
+	{
+		ncpy /= 10;
+		len++;
+	}
+	while (n != 0)
+	{
+		remainder = n % 10;
+		reverse = reverse * 10 + remainder;
+		n /= 10;
+	}
+	while (reverse > 0)
+	{
+		digit = reverse % 10;
+		_putchar (digit + '0');
+		reverse /= 10;
 		i++;
 	}
-	if (nums > 0)
+	while (i++ < len)
+		_putchar('0');
+	return (len);
+}
+/**
+ * print_unsigned - ...
+ * @ap: va_list
+ * Return: len of unsigned int
+ */
+int print_unsigned(va_list ap)
+{
+	int len = 0;
+	long x = va_arg(ap, long);
+	char *negNum = "4288073002";
+
+	if (x < 0)
 	{
-		while (nums / 10 != 0)
+		while (*negNum != '\0')
 		{
-			exp = exp * 10;
-			nums = nums / 10;
-		}
-		nums = n;
-		while (exp > 0)
-		{
-			digit = nums / exp;
-			_putchar(digit + '0');
-			nums = nums - (digit * exp);
-			exp = exp / 10;
-			i++;
+			_putchar(*negNum);
+			negNum++;
+			len++;
 		}
 	}
-	_putchar(last + '0');
+	else
+	{
+		unsigned int n = (unsigned int) x;
 
-	return (i);
+		len += print_unsigned_help(n);
+	}
+	return (len);
 }
