c3c0939e2d0bd911c8e59a3f3bb556ed5ebd2526
asmaaMahmoudF <asmaafahmy932@gmail.com>
2023-10-19 19:19:55 +0300

    “asss”

--- _printf.c
+++ _printf.c
@@ -7,6 +7,7 @@
 int _printf(const char *format, ...)
 {
 	int j, i = 0, len = 0;
+	int match = 0;
 	va_list ap;
 	convert_match fun[] = {{'c', printf_char}, {'s', printf_string},
 		{'%', print_mod}, {'i', printf_int},
@@ -14,22 +15,34 @@ int _printf(const char *format, ...)
 	va_start(ap, format);
 	if ((!format) || (format[0] == '%' && !format[1]))
 		return (-1);
-	while (format[i])
+	while (format[i] && format)
 	{
-		if (format[i] == '%')
+		if (format[i] == '%' && format[i + 1]) 
 		{
-			if (format[i + 1] == '\0' || format[i  + 1] == '!')
+			if (format[i + 1] == '\0')
 				return (-1);
-			j = 0;
-			while (j < 6)
+			if (format[i + 1] != '\0')
 			{
-				if (format[i + 1] == fun[j].ch)
-				{
-					len += fun[j].f(ap);
-					i += 2;
-					break;
-				}
-				j++;
+			    j = 0;
+				match = 0;
+			    while (j < 6)
+			       {
+			    	 if (format[i + 1] == fun[j].ch)
+			        	{
+				        	len += fun[j].f(ap);
+				        	i += 2;
+							match = 1;
+				        	break;
+				        }
+				    j++;
+		        	}
+				if (!match)
+					{
+						_putchar('%');
+						_putchar(format[i + 1]);
+						len += 2;
+						i += 2;
+					}
 			}
 		}
 		else
